9649a3938d6907ba6e405493c020d51c205869d1
vivichung <vivichung0201@gmail.com>
2026-04-12 22:34:16 -0400

    attached model to front end, still need some slight formatting tweaks

--- src/app.py
+++ src/app.py
@@ -1,10 +1,28 @@
 import io
+import os
+import json
 from flask import Flask, render_template, request, jsonify
 from PIL import Image
 import base64
-from model import run_model
+import tensorflow as tf
+import numpy as np
 
 app = Flask(__name__)
+
+# load model at startup
+MODEL_PATH = os.path.join(os.path.dirname(__file__), '..', 'model', 'plant_disease_cnn.keras')
+LABEL_PATH = os.path.join(os.path.dirname(__file__), '..', 'model', 'label_map.json')
+TARGET_SIZE = (224, 224)
+
+TF_MODEL = None
+LABELS = []
+if os.path.exists(LABEL_PATH):
+    with open(LABEL_PATH, 'r') as f:
+        LABELS = json.load(f).get('classes', [])
+if os.path.exists(MODEL_PATH):
+    TF_MODEL = tf.keras.models.load_model(MODEL_PATH)
+else:
+    print("Warning: TF model not found at", MODEL_PATH)
 
 #using png as the deafult file type --> can change if needed
 def encoding(image:Image.Image, fmt="PNG") -> str:
@@ -24,24 +42,41 @@ def hello_world():
 
 @app.route("/prediction", methods = ["POST"])
 def prediction():
+    if TF_MODEL is None:
+        return jsonify({'error': 'model not loaded'}), 500
+    f = request.files.get('image')
+    if not f:
+        return jsonify({'error': 'no file uploaded'}), 400
+    try:
+        img = Image.open(io.BytesIO(f.read())).convert('RGB')
+        img = img.resize(TARGET_SIZE, Image.BILINEAR)
+        arr = np.asarray(img).astype('float32') / 255.0
+        x = np.expand_dims(arr, 0)
+        preds = TF_MODEL.predict(x)[0]
+        idx = int(np.argmax(preds))
+        if float(preds[idx]) < 0.8:
+            return jsonify({'error':'Photo Does Not Match One of the Classifications'}),  500
+        return jsonify({'class': LABELS[idx] if LABELS else str(idx), 'confidence': "{(preds[idx]*100):.2f}%"})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
     #need to call model here 
 
-    if "image" not in request.files:
-        return TypeError
+    # if "image" not in request.files:
+    #     return TypeError
     
-    file = request.files["image"]
+    # file = request.files["image"]
 
-    if file.filename not in allowed_ext(file.filename):
-        return TypeError
+    # if file.filename not in allowed_ext(file.filename):
+    #     return TypeError
     
-    image = Image.open(file.stream)
+    # image = Image.open(file.stream)
 
-    #output = run_model(encoded_image) 
-    #need to gte the model function and need to see what dtype is returned
+    # #output = run_model(encoded_image) 
+    # #need to gte the model function and need to see what dtype is returned
 
-    #return render_template("index.html", output)
-    return render_template("index.html")
+    # #return render_template("index.html", output)
+    # return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- src/gettingkaggle.py
+++ src/gettingkaggle.py
@@ -19,24 +19,6 @@ healthynums = random.sample(range(1, 35134), subsetcount)
 pestsnums = random.sample(range(1, 34790), subsetcount)
 virusnums = random.sample(range(1, 35291), subsetcount)
 i = 0
-while i < subsetcount: 
-    # print('pathogens/bacteria/bac ('+str(num)+').JPG')  <---------- was to test the numbers
-    try:
-        kaggle.api.dataset_download_file(dataset,'pathogen/bacteria/bac ('+str(bacterianums[i])+').JPG', path='./randomdata')    
-        kaggle.api.dataset_download_file(dataset,'pathogen/fungus/fung ('+str(fungusnums[i])+').JPG', path='./randomdata')
-        kaggle.api.dataset_download_file(dataset,'pathogen/healthy/hea ('+str(healthynums[i])+').JPG', path='./randomdata')
-        kaggle.api.dataset_download_file(dataset,'pathogen/pests/pes ('+str(pestsnums[i])+').JPG', path='./randomdata')
-        kaggle.api.dataset_download_file(dataset,'pathogen/virus/vir ('+str(virusnums[i])+').JPG', path='./randomdata')
-        i+=1
-    except Exception as e:
-        print("bad number!")
-        bacterianums[i] = random.randint(1, 35423)
-        fungusnums[i] = random.randint(1, 36076)
-        healthynums[i] = random.randint(1, 35134)
-        pestsnums[i] = random.randint(1, 34790)
-        virusnums[i] = random.randint(1, 35291)
-
-
 while i < subsetcount:
     try:
         # print('pathogen/bacteria/bac ('+str(bacterianums[i])+').JPG')
@@ -75,9 +57,9 @@ while i < subsetcount:
 i = 0
 while i < subsetcount:
     try:
-        print('pathogen/virus/vir ('+str(virusnums[i])+').JPG')
+        #print('pathogen/virus/vir ('+str(virusnums[i])+').JPG')
         kaggle.api.dataset_download_file(dataset,'pathogen/virus/vir ('+str(virusnums[i])+').JPG', path='./randomdata/vir')
         i+=1
     except Exception as e:
-        print("bad vir number!")
+        #print("bad vir number!")
         virusnums[i] = random.randint(1, 35291)
